bb057fb1c16d19af959ce2328a3d040a61158158
Addin Gama Bertaqwa <punyagama@gmail.com>
2019-03-28 06:45:29 +0800

    Fetch list id

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final bloc = StoriesProvider.of(context);
+
+    // Temporary code, bad code
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show some news here'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still waiting on Ids');
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text('${snapshot.data[index]}');
+          },
+        );
+      },
     );
   }
 }
