ad4f8fa4bb1295d1da94dbb5c224c2fb6f755fa1
Addin Gama Bertaqwa <punyagama@gmail.com>
2019-03-28 09:31:34 +0800

    Handle loading state of news list tile

--- lib/src/widgets/news_list_tile.dart
+++ lib/src/widgets/news_list_tile.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 import '../blocs/stories_provider.dart';
+
 
 class NewsListTile extends StatelessWidget {
   final int itemId;
@@ -11,6 +13,13 @@ class NewsListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = StoriesProvider.of(context);
 
-    return null;
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Stream still loading');
+        }
+      },
+    );
   }
 }
